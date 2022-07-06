a2817738bcc6224843ff7e8610245d0497e55ae0
Salman <maker57sk@gmail.com>
2022-07-06 10:44:40 +0400

    new datasets added

--- aimluae/datasets.py
+++ aimluae/datasets.py
@@ -18,8 +18,8 @@ def get_data(data_set):
 
 def list_datasets():
    data_sets = [
-      'churn', 'courier_companies', 'customs_warehouse_licensees',
-      'customs_warehouses', 'declaration_cargo_detail-1', 'declaration_cargo_detail-2', 
-      'livebirths', 'new_sales', 'Services-En_0', 'vehicle_clearance_certificate'
+      'courier_companies', 'customs_warehouse_licensees', 'Number of teaching staff_in_Higher_Education_2020_06_09', 
+      'customs_warehouses', 'declaration_cargo_detail-1', 'declaration_cargo_detail-2', 'Graduates_in_Highe_ Education_20-06-09',
+      'livebirths', 'Services-En_0', 'vehicle_clearance_certificate', 'UAE_National_Red List_Species',
    ]
    return data_sets
